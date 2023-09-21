3ba26e319723ca2da064bcefaed7047f5cdbcca9
Divyansh Sevta <dsevta@gmail.com>
2023-09-21 22:15:56 +0530

    added authentication

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", LandingPageView.as_view(), name="landing-page"),
     path("leads/", include("leads.urls", namespace="leads")),
+    path("login/", LoginView.as_view(), name="login"),
 ]
 
 if settings.DEBUG:
