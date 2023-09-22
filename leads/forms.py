b970fdbb638a9765e498989b1fe047938fd781aa
Divyansh Sevta <dsevta@gmail.com>
2023-09-22 10:50:54 +0530

    added CustomUserCreationForm

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth import get_user_model
 from django import forms
 from .models import Lead
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {"username": UsernameField}
 
 
 class LeadModelForm(forms.ModelForm):
