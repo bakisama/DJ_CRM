67341dcd07a645b17fa5b4318fa6a7ddd081064f
Divyansh Sevta <dsevta@gmail.com>
2023-09-26 17:54:57 +0530

    changed agent create view

--- agents/views.py
+++ agents/views.py
@@ -27,8 +27,18 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(user=user, organisation=self.request.user.userprofile)
+        send_mail(
+            subject="You are invited to be an agent",
+            message="You were added as an agent on DJCRM. Please login to start working",
+            from_email="toska@gmail.com",
+            recipient_list=[user.email],
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 
