8ce65311f03939919d6e1bfe70c57d1246002da7
Divyansh Sevta <dsevta@gmail.com>
2023-09-26 17:55:30 +0530

    ;-;

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -117,7 +117,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "leads.User"
 
-EMAIL_BACKEND = "django.core.mail.backend.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGIN_REDIRECT_URL = "/leads"
 
