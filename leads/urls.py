3141859c7b36d3075ceefd57ffa0628a10ba28ad
Divyansh Sevta <dsevta@gmail.com>
2023-09-26 17:59:25 +0530

    added AssignAgent, CategoryList and CategoryDetail

--- leads/urls.py
+++ leads/urls.py
@@ -5,6 +5,9 @@ from .views import (
     LeadCreateView,
     LeadUpdateView,
     LeadDeleteView,
+    AssignAgentView,
+    CategoryListView,
+    CategoryDetailView,
 )
 
 app_name = "leads"
@@ -15,4 +18,11 @@ urlpatterns = [
     path("<int:pk>/update/", LeadUpdateView.as_view(), name="lead-update"),
     path("<int:pk>/delete/", LeadDeleteView.as_view(), name="lead-delete"),
     path("create/", LeadCreateView.as_view(), name="lead-create"),
+    path("<int:pk>/assign-agent/", AssignAgentView.as_view(), name="assign-agent"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path(
+        "categories/<int:pk>/",
+        CategoryDetailView.as_view(),
+        name="category-detail",
+    ),
 ]
